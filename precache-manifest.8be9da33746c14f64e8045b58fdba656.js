1142c98da2d179721eb37a5a605abb3224d657e3
silvio alzate <silvioalzate@gmail.com>
2020-10-23 13:12:13 -0500

    deploy

---
+++ precache-manifest.8be9da33746c14f64e8045b58fdba656.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813f9216a82502a2f57d",
+    "url": "/css/app.a8c6970e.css"
+  },
+  {
+    "revision": "84e8a3e12ff566da0fb2",
+    "url": "/css/data.b3672ca5.css"
+  },
+  {
+    "revision": "574f85d78f4929ed35e8",
+    "url": "/css/dimensions.4856bfa0.css"
+  },
+  {
+    "revision": "606ac8c85d1a7ee542f5",
+    "url": "/css/final.80ceab26.css"
+  },
+  {
+    "revision": "daf62637dda458f0e03d",
+    "url": "/css/login.02c7a1b3.css"
+  },
+  {
+    "revision": "b27823e50713d7cbeae8",
+    "url": "/css/payment.8ba950f4.css"
+  },
+  {
+    "revision": "174dac350e3d6f4156e6",
+    "url": "/css/upload.8e455a56.css"
+  },
+  {
+    "revision": "5e055714d2c647827907bbf144cf409f",
+    "url": "/img/logo.5e055714.png"
+  },
+  {
+    "revision": "675880dd2d58353b52c119fa24b6e98f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "813f9216a82502a2f57d",
+    "url": "/js/app.21bf0889.js"
+  },
+  {
+    "revision": "f575dcc59a6f3f00b492",
+    "url": "/js/chunk-vendors.4e918611.js"
+  },
+  {
+    "revision": "84e8a3e12ff566da0fb2",
+    "url": "/js/data.1afdf09b.js"
+  },
+  {
+    "revision": "574f85d78f4929ed35e8",
+    "url": "/js/dimensions.bd809f22.js"
+  },
+  {
+    "revision": "606ac8c85d1a7ee542f5",
+    "url": "/js/final.d6476623.js"
+  },
+  {
+    "revision": "daf62637dda458f0e03d",
+    "url": "/js/login.bb0b33f8.js"
+  },
+  {
+    "revision": "b27823e50713d7cbeae8",
+    "url": "/js/payment.45dc6447.js"
+  },
+  {
+    "revision": "174dac350e3d6f4156e6",
+    "url": "/js/upload.e12a0e71.js"
+  },
+  {
+    "revision": "14fa580a1660affc96d50bd134a903fc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
